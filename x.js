6d7b09cc6e00ea7df30a88f3a269b9fd05c8bc31
nick lehman <nick.lehman@gmail.com>
2014-12-04 14:55:09 -0500

    scrape 3 pages. todo loop til done

--- x.js
+++ x.js
@@ -8,6 +8,22 @@
 		clientScripts:['./jquery.csv.js']
 	}), members = [];
 
+	function scrape() {
+		var x=$('#dnn_ctr699_ViewDynamicUser_dlUsers tr'), member = [];
+		x=x.slice(1, x.length-1);
+		x.each(function(i, v) {
+			var e = $('h2', v), telEmailWeb = e.next().next().text().trim().split(/\n/);
+			member.push({
+				name : e.text().trim(),
+				addr : e.next().text().trim(),
+				tel : telEmailWeb[0],
+				email : telEmailWeb[1],
+				www : telEmailWeb[2]
+			});
+		});
+		return member;
+	}
+
 	casper.start('http://www.mava.org/Membership/MAVA-Members.aspx', function() {
 		casper.evaluate(function(sel, val) {
 			$(sel).find("option:contains('" + val + "')")
@@ -17,27 +33,12 @@
 	});
 
 	casper.then(function() {
-		members.push(casper.evaluate(function() {
-			var x=$('#dnn_ctr699_ViewDynamicUser_dlUsers tr'), member = [];
-			x=x.slice(1, x.length-1);
-			x.each(function(i, v) {
-				var e = $('h2', v), telEmailWeb = e.next().next().text().trim().split(/\n/);
-				member.push({
-					name : e.text().trim(),
-					addr : e.next().text().trim(),
-					tel : telEmailWeb[0],
-					email : telEmailWeb[1],
-					www : telEmailWeb[2]
-				});
-			});
-			// return as CSV
-			return $.csv.fromObjects(member);
-		}));
-		console.log(members);
-	}).then(function() {
-		this.click('a#dnn_ctr699_ViewDynamicUser_lbNext');
-	}).then(function() {
-		casper.capture('shot.png');
+		members = members.concat(casper.evaluate(scrape));
+	}).thenClick('a#dnn_ctr699_ViewDynamicUser_lbNext', function() {
+		members = members.concat(casper.evaluate(scrape));
+	}).thenClick('a#dnn_ctr699_ViewDynamicUser_lbNext', function() {
+		members = members.concat(casper.evaluate(scrape));
+		console.log(members.length);
 	});
 
 	casper.run();
